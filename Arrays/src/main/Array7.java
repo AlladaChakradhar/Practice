6065622a436c0032bdf1a4018617131af5309222
Chakri <chakradharallada@gmail.com>
2025-09-14 17:32:22 +0530

    Refactored code.

--- Arrays/src/main/Array7.java
+++ Arrays/src/main/Array7.java
@@ -15,35 +15,16 @@ public class Array7 {
 		int[] nums = { 7, 3, 2, 4, 9, 12, 56 };
 		Scanner sc = new Scanner(System.in);
 		int k = sc.nextInt();
-		int[] differ = new int[nums.length + 1 - k];
+		int dmin=Integer.MAX_VALUE;
 		for (int i = 0; i <= nums.length - k; i++) {
-//			System.out.print("[");
 			int max = 0;
 			int min = nums[i];
 			for (int j = i; j < i + k; j++) {
-				int diff = 0;
-//				System.out.print(nums[j]);
-//				if (j < i + k - 1) {
-//					System.out.print(",");
-//					break;
-//				}
-				if (nums[j] > max) {
-					max = nums[j];
-				}
-				if (min >= nums[j]) {
-					min = nums[j];
-				}
+				if (nums[j] > max) max = nums[j];
+				if (min >= nums[j])min = nums[j];
 			}
 			int diff = max - min;
-			differ[i] = diff;
-//			System.out.println("] ---> diff :" + diff);
-		}
-
-		int dmin = differ[0];
-		for (int i = 0; i < differ.length; i++) {
-			if (dmin >= differ[i]) {
-				dmin = differ[i];
-			}
+			if(diff<dmin) dmin=diff;
 		}
 		System.out.println(dmin);
 
